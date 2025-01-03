873e9cd8f413c20968de1fc17148720d99fd50ce
daniel360kim <nmrs.thrust@gmail.com>
2025-01-03 16:12:23 +0100

    loading indicator while timerfx loading

--- lib/studyroom/widgets/screens/timer/timer.dart
+++ lib/studyroom/widgets/screens/timer/timer.dart
@@ -1,4 +1,5 @@
 import 'package:card_swiper/card_swiper.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:studybeats/api/timer_fx/objects.dart';
 import 'package:studybeats/api/timer_fx/timer_fx_service.dart';
 import 'package:studybeats/colors.dart';
@@ -263,84 +264,107 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
   }
 
   Widget buildSoundFxSelector() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        IconButton(
-          onPressed: () {
-            setState(() {
-              _enableTimerSound = !_enableTimerSound;
-            });
-            widget.onTimerSoundEnabled(_enableTimerSound);
-          },
-          icon: Icon(
-            _enableTimerSound
-                ? Icons.notifications_active
-                : Icons.notifications_off,
-            color: kFlourishBlackish,
-          ),
-        ),
-        const SizedBox(width: 10),
-        if (_enableTimerSound)
-          Container(
-            height: 30,
-            width: 170,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              color: kFlourishLightBlackish,
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: kFlourishBlackish.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-              border: Border.all(
-                color: kFlourishBlackish.withOpacity(0.1),
-              ),
+    if (_enableTimerSound) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _enableTimerSound = !_enableTimerSound;
+              });
+              widget.onTimerSoundEnabled(_enableTimerSound);
+            },
+            icon: Icon(
+              _enableTimerSound
+                  ? Icons.notifications_active
+                  : Icons.notifications_off,
+              color: kFlourishBlackish,
             ),
-            child: DropdownButton<TimerFxData>(
-                value: _selectedTimerFx,
-                isExpanded: true,
-                icon: const Icon(Icons.arrow_drop_down),
-                iconSize: 24,
-                elevation: 16,
-                underline: Container(
-                  height: 0,
-                  color: Colors.transparent,
-                ),
-                items: _timerFxList
-                    .map((TimerFxData timerFxData) =>
-                        DropdownMenuItem<TimerFxData>(
-                          value: timerFxData,
-                          child: Row(
-                            children: [
-                              Text(
-                                timerFxData.name,
-                                style: GoogleFonts.inter(
-                                  color: kFlourishBlackish,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ))
-                    .toList(),
-                onChanged: (TimerFxData? timerFxData) {
-                  if (timerFxData == null) return;
-                  setState(() {
-                    _selectedTimerFx = timerFxData;
-                  });
-
-                  _timerPlayer.playTimerSound(timerFxData);
-                  widget.onTimerSoundSelected(timerFxData);
-                }),
           ),
-      ],
-    );
+          const SizedBox(width: 10),
+          _timerFxList.isEmpty
+              ? const SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: CircularProgressIndicator(
+                    color: kFlourishBlackish,
+                  ),
+                )
+              : Container(
+                  height: 30,
+                  width: 170,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: kFlourishLightBlackish,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: kFlourishBlackish.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                    border: Border.all(
+                      color: kFlourishBlackish.withOpacity(0.1),
+                    ),
+                  ),
+                  child: DropdownButton<TimerFxData>(
+                    value: _selectedTimerFx,
+                    isExpanded: true,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    iconSize: 24,
+                    elevation: 16,
+                    underline: Container(
+                      height: 0,
+                      color: Colors.transparent,
+                    ),
+                    items: _timerFxList
+                        .map((TimerFxData timerFxData) =>
+                            DropdownMenuItem<TimerFxData>(
+                              value: timerFxData,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    timerFxData.name,
+                                    style: GoogleFonts.inter(
+                                      color: kFlourishBlackish,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ))
+                        .toList(),
+                    onChanged: (TimerFxData? timerFxData) {
+                      if (timerFxData == null) return;
+                      setState(() {
+                        _selectedTimerFx = timerFxData;
+                      });
+
+                      _timerPlayer.playTimerSound(timerFxData);
+                      widget.onTimerSoundSelected(timerFxData);
+                    },
+                  ),
+                ),
+        ],
+      );
+    } else {
+      return IconButton(
+        onPressed: () {
+          setState(() {
+            _enableTimerSound = !_enableTimerSound;
+          });
+          widget.onTimerSoundEnabled(_enableTimerSound);
+        },
+        icon: Icon(
+          Icons.notifications_off,
+          color: kFlourishBlackish,
+        ),
+      );
+    }
   }
 }
 
